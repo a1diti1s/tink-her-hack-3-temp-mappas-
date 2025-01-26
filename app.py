b9af8186a7fa2e5e25ee644aa46093c9f660f9dd
Aleena Sebastian <aleenasebastian311@gmail.com>
2025-01-26 08:51:48 +0530

    Added playlist

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/playlist')
+def playlist():
+    return render_template('playlist.html')
+
 @app.route('/search', methods=['GET'])
 def search():
     query = request.args.get('query')
